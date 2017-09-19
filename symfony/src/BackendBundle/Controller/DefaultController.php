e1124843379f9f4fd0b14a976645f7cbf35a3f87
deivisjl <deivisjl@gmail.com>
2017-09-19 05:56:22 -0600

    Devolver json
    Implementando metodo para devolver json en el defaultController

--- symfony/src/BackendBundle/Controller/DefaultController.php
+++ symfony/src/BackendBundle/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace BackendBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 class DefaultController extends Controller
 {
@@ -22,8 +26,25 @@ class DefaultController extends Controller
 
         $users = $em->getRepository('BackendBundle:User')->findAll();
 
-        var_dump($users);
+        return $this->json($users);
+    }
 
-        die();
+    public function json($data){
+
+    	$normalizers = array(new GetSetMethodNormalizer);
+
+    	$encoders = array("json" => new JsonEncoder())	;
+
+    	$serializer = new Serializer($normalizers,$encoders);
+
+    	$json = $serializer->serialize($data,'json');
+
+    	$response = new Response();
+
+    	$response->setContent($json);
+
+    	$response->headers->set("Content-Type","application/json");
+
+    	return $response;
     }
 }
