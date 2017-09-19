72e3f68009b00c585a15e5d61bb48ae752cc32a6
deivisjl <deivisjl@gmail.com>
2017-09-19 06:22:29 -0600

    Ruta y accion Login
    Ruta para el metodo login del api

--- symfony/src/BackendBundle/Controller/DefaultController.php
+++ symfony/src/BackendBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace BackendBundle\Controller;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -29,6 +30,44 @@ class DefaultController extends Controller
         $users = $em->getRepository('BackendBundle:User')->findAll();
 
         return $helpers->json($users);
+    }
+
+    public function loginAction(Request $request){
+
+    	$helpers = $this->get("app.helpers");
+
+    	$json = $request->get("json",null);
+
+
+    	if ($json != null) {
+    		
+    		$params = json_decode($json);
+
+    		$email = (isset($params->email)) ? $params->email : null;
+
+    		$password = (isset($params->password)) ? $params->password : null;
+
+    		$emailConstraint = new Assert\Email();
+
+    		$emailConstraint->message = "This email is not valid!!";
+
+    		$validate_email = $this->get("validator")->validate($email,$emailConstraint);
+
+    		if (count($validate_email) == 0 && $password != null) {
+    			
+    			echo "Data success!!";
+    		}else{
+
+    			echo "Data incorrect!!";
+    		}
+
+
+    	}else{
+
+    		echo "Send json with post!";
+    	}
+
+    	die();
     }
 
     
