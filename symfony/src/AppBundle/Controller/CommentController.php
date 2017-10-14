4de9915a1cae9c506089202af5af44780974bf0d
deivisjl <deivisjl@gmail.com>
2017-10-14 06:34:35 -0600

    Creacion de controlador de comentarios
    Implementacion de cambios para el funcionamiento del controlador de
    comentarios

---
+++ symfony/src/AppBundle/Controller/CommentController.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use BackendBundle\Entity\User;
+use BackendBundle\Entity\Video;
+
+/**
+* 
+*/
+class CommentController extends Controller
+{
+
+	public function newAction(){
+		
+		
+	}
+}
