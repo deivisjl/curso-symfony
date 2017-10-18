c7aebe5a0cfae65888692b8c56fd6477240e43dd
deivisjl <deivisjl@gmail.com>
2017-10-18 07:30:31 -0600

    Formulario de login
    Estructuracion de la vista del formulario de login

--- angular/app/components/login.component.ts
+++ angular/app/components/login.component.ts
@@ -4,8 +4,10 @@ import {Component, OnInit} from '@angular/core';
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'login',
-    template: `<h1>Formulario de Login</h1>`
+    templateUrl: 'app/view/login.html'
 })
  
 // Clase del componente donde irán los datos y funcionalidades
-export class LoginComponent { }
+export class LoginComponent { 
+	public titulo: string = "Iniciar sesion";
+}
