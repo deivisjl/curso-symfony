fb5d064902b242e66585fd411c3d899a25353b6c
deivisjl <deivisjl@gmail.com>
2017-10-19 08:50:04 -0600

    Finalizando menu de usuario
    Finalizando menu principal para su acceso

--- angular/app/components/default.component.ts
+++ angular/app/components/default.component.ts
@@ -1,11 +1,27 @@
 // Importar el núcleo de Angular
 import {Component, OnInit} from '@angular/core';
+import { ROUTER_DIRECTIVES, Router, ActivatedRoute } from '@angular/router';
+import { LoginService } from '../services/login.service';
+
  
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'default',
-    template: `<h1>Componente por defecto</h1>`
+    templateUrl: 'app/view/default.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [LoginService]
 })
  
 // Clase del componente donde irán los datos y funcionalidades
-export class DefaultComponent { }
+export class DefaultComponent { 
+	public titulo = "Portada";
+	public identity;
+
+	constructor(private _loginService: LoginService){}
+
+	ngOnInit(){
+
+		this.identity = this._loginService.getIdentity();
+
+	}
+}
