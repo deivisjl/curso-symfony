174b99642a49b7193caa292acecb0047b7d7fee4
deivisjl <deivisjl@gmail.com>
2017-10-24 07:32:21 -0600

    Creacion de modelo video
    Creando la clase video para contener un modelo de datos

---
+++ angular/app/model/video.ts
@@ -0,0 +1,14 @@
+export class Video{
+	constructor(
+			public id:number,
+			public titulo:string,
+			public description:string,
+			public status:string,
+			public image:string,
+			public videoPath,
+			public createdAt,
+			public updatedAt
+		){
+
+	}
+}
