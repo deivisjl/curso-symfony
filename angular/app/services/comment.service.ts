e864dcd9d185771513a918f95dbda3bcab1798a9
deivisjl <deivisjl@gmail.com>
2017-11-09 08:07:19 -0600

    Agregar comentario
    Conectado el componente de comentario hacia el servicio

---
+++ angular/app/services/comment.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { Http, Response, Headers } from "@angular/http";
+import "rxjs/add/operator/map";
+import { Observable } from "rxjs/Observable";
+
+@Injectable()
+export class CommentService{
+
+	public url = "http://www.symfonyapi.com/symfony/web/app_dev.php";
+
+	constructor(private _http:Http){}
+
+	create(token, comment){
+
+		let json = JSON.stringify(comment);
+		let params = "json=" + json + "&authorization="+token;
+		let headers = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
+
+		return this._http.post(this.url + "/comment/new", params,{headers: headers})
+							.map(res => res.json());
+	}
+}
